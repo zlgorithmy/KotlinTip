d32185bdb80d720e776f4bcc4f7e750028c3770b
zlgorithmy <zlgorithmy@gmail.com>
2019-08-29 19:40:35 +0800

    add spinner

--- app/src/main/java/com/zlgorithmy/kotlintip/ControlActivity.kt
+++ app/src/main/java/com/zlgorithmy/kotlintip/ControlActivity.kt
@@ -1,7 +1,11 @@
 package com.zlgorithmy.kotlintip
 
+import android.content.DialogInterface
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
@@ -11,13 +15,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.zlgorithmy.kotlintip.adapter.KotlinRecycleAdapter
 import com.zlgorithmy.kotlintip.setting.SettingsActivity
 import kotlinx.android.synthetic.main.activity_control.*
+import org.jetbrains.anko.selector
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class ControlActivity : AppCompatActivity() {
+    private lateinit var mItems: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
+        mItems = arrayOf(
+            resources.getString(R.string.one),
+            resources.getString(R.string.two),
+            resources.getString(R.string.three)
+        ).toList()
         initView()
     }
 
@@ -35,7 +47,7 @@ class ControlActivity : AppCompatActivity() {
 
         // itemDecoration
         val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        itemDecoration.setDrawable(resources.getDrawable(R.drawable.king_yna,null))
+        itemDecoration.setDrawable(resources.getDrawable(R.drawable.king_yna, null))
         mRecyclerView.addItemDecoration(itemDecoration)
 
         // animation
@@ -72,10 +84,38 @@ class ControlActivity : AppCompatActivity() {
         videoView?.setOnCompletionListener { videoView?.start() }
     }
 
+    private fun initSpinner() {
+        //val sp = findViewById<View>(R.id.spinner) as Spinner
+        val startAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, mItems)
+        startAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        spinner?.prompt = "请选择"
+        spinner?.adapter = startAdapter
+        //sp.setSelection(0)
+        spinner?.onItemSelectedListener = myItemClickListener()
+
+        spinnerText?.text = resources.getString(R.string.TextView)
+        spinnerText?.setOnClickListener {
+            selector("请选择", mItems) { _: DialogInterface, i: Int ->
+                toast("你的选择是：${mItems[i]}")
+            }
+        }
+    }
+
     private fun initView() {
         initBtn()
         initRecyclerView()
         initWeb()
         initVideo()
+        initSpinner()
+    }
+
+    internal inner class myItemClickListener : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+
+        }
+
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            toast("你的选择是：${mItems[position]}")
+        }
     }
 }
